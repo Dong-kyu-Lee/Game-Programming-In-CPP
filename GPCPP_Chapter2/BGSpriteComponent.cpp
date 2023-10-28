0d2090f1e5b19eb6e91a0d1d96927ea0d5b17f8b
Dong-Kyu-Lee <stvboy00@gmail.com>
2023-10-28 23:42:26 +0900

    BGSpriteComponent의 Update, SetTextures 구현

--- GPCPP_Chapter2/BGSpriteComponent.cpp
+++ GPCPP_Chapter2/BGSpriteComponent.cpp
@@ -8,6 +8,18 @@ BGSpriteComponent::BGSpriteComponent(Actor* owner, int drawOrder)
 
 void BGSpriteComponent::Update(float deltaTime)
 {
+	SpriteComponent::Update(deltaTime);
+
+	// x의 오프셋 값 갱신
+	for (auto bg : mBGTextures)
+	{
+		bg.mOffset.x -= mScrollSpeed * deltaTime;
+
+		if (bg.mOffset.x < -mScreenSize.x)
+		{
+			bg.mOffset.x = (mBGTextures.size() - 1) * mScreenSize - 1;
+		}
+	}
 }
 
 void BGSpriteComponent::Draw(SDL_Renderer* renderer)
@@ -16,5 +28,15 @@ void BGSpriteComponent::Draw(SDL_Renderer* renderer)
 
 void BGSpriteComponent::SetBGTextures(const std::vector<SDL_Texture*>& textures)
 {
-
+	int count = 0;
+	for (auto tex : textures)
+	{
+		BGTexture temp;
+		temp.mTexture = tex;
+		// 각 텍스처의 오프셋은 화면의 너비 * count다.
+		temp.mOffset.x = mScreenSize.x * count;
+		temp.mOffset.y = 0;
+		mBGTextures.emplace_back(temp);
+		count++;
+	}
 }
