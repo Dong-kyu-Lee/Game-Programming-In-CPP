a9f38093827198023cdaf43d1887b0af263f7cf9
Dong Kyu Lee <stvboy00@gmail.com>
2023-11-30 16:20:27 +0900

    Created AI states : Patrol, Death, Attack

--- GPCPP_Chapter4/AIState.h
+++ GPCPP_Chapter4/AIState.h
@@ -1,8 +1,10 @@
 #pragma once
+#include "AIComponent.h"
+
 class AIState
 {
 public:
-	AIState(class AIComponent* owner)
+	AIState(AIComponent* owner)
 		: mOwner(owner)
 	{}
 	// 각 상태의 구체적인 행동
@@ -13,6 +15,71 @@ public:
 	virtual const char* GetName() const = 0;
 
 protected:
-	class AIComponent* mOwner;
+	AIComponent* mOwner;
 };
 
+class AIPatrol :
+	public AIState
+{
+public:
+	AIPatrol(class AIComponent* owner);
+
+	// 이 상태에 대한 행위 재정의
+	void Update(float deltaTime) override;
+	void OnEnter() override;
+	void OnExit() override;
+
+	const char* GetName() const override
+	{
+		return "Patrol";
+	}
+};
+
+class AIDeath :
+	public AIState
+{
+public:
+	AIDeath(class AIComponent* owner);
+
+	// 이 상태에 대한 행위 재정의
+	void Update(float deltaTime) override;
+	void OnEnter() override;
+	void OnExit() override;
+
+	const char* GetName() const override
+	{
+		return "Death";
+	}
+};
+
+class AIAttack :
+	public AIState
+{
+public:
+	AIAttack(class AIComponent* owner);
+
+	// 이 상태에 대한 행위 재정의
+	void Update(float deltaTime) override;
+	void OnEnter() override;
+	void OnExit() override;
+
+	const char* GetName() const override
+	{
+		return "Attack";
+	}
+};
+
+struct WeightedGraphEdge
+{
+	// 간선에 어떤 노드가 연결되어 있는가?
+	WeightedGraphNode* mFrom;
+	WeightedGraphNode* mTo;
+	// 간선의 가중치
+	float mWeight;
+};
+
+struct WeightedGraphNode
+{
+	// 외부로 향하는 간선들를 저장한다.
+	std::vector<WeightedGraphEdge*> mEdges;
+};
